c6c56f37c0950fb5e76059edf560ff43228be0ec
ValentinoBestak <valentino.bestak@gmail.com>
2020-10-08 14:44:53 +0200

    hamburger animation

--- app/js/index.js
+++ app/js/index.js
@@ -1,2 +1,9 @@
 const hamburgerMenu = document.querySelector(".header__hamburger");
 
+hamburgerMenu.addEventListener("click", ()=>{
+    if(hamburgerMenu.classList.contains("open")){
+        hamburgerMenu.classList.remove("open")
+    }else{
+        hamburgerMenu.classList.add("open") 
+    }
+});
